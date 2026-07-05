f6b2ebfe373b2f684e297434d5a02e4aece4042f
Claire Vivian de Lunette <whotookclaireee@gmail.com>
2026-07-05 17:43:45 +0800

    coding is fun debugging isnt

--- source/Strain.hx
+++ source/Strain.hx
@@ -1,9 +1,29 @@
 package source;
 
 public class Strain {
-    public static function getStrainOfStrumline(strumline:Dynamic):Float {
-        var individualStrains = new Array(strumLine.members.length);
+    public static function getStrainOfStrumline(strumline:Dynamic):Dynamic {
+        var toReturn:Dynamic = {average: 0, laneStrains: []}
 
-        trace(individualStrains);
+        var individualStrains:Array<Float> = [];
+        var lastNote:Array<Dynamic> = [];
+        for (i in 0...strumline.members.length) {
+            individualStrains.push(0);
+            lastNote.push(null);
+        }
+
+        for (note in strumline.notes.members) {
+            if (lastNote[note.id] == null) {
+                lastNote[note.id] = note;
+                continue;
+            }
+
+            individualStrains[note.id] += (1 / Math.abs((note.time - (lastNote[note.id].time + lastNote[note.id].slen))));
+        }
+
+        toReturn.laneStrains = individualStrains;
+        for (i in individualStrains) toReturn.average += i;
+        toReturn.average /= individualStrains.length;
+
+        return toReturn;
     }
 }
