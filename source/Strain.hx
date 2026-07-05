ce30d41101ff994f6c7429a41253d2b408121c80
Claire Vivian de Lunette <whotookclaireee@gmail.com>
2026-07-05 17:31:27 +0800

    strain calculations TrollDespair

---
+++ source/Strain.hx
@@ -0,0 +1,9 @@
+package source;
+
+public class Strain {
+    public static function getStrainOfStrumline(strumline:Dynamic):Float {
+        var individualStrains = new Array(strumLine.members.length);
+
+        trace(individualStrains);
+    }
+}
